48f6f53fa6fbd623bca61f079b803fe4449a0b63
yoejoder <112516718+yoejoder@users.noreply.github.com>
2024-04-10 11:43:55 -0600

    Update CF2Game.py

--- CF2Game.py
+++ CF2Game.py
@@ -1,1 +1,23 @@
-import pygame
+import pygame, sys
+from pygame.locals import *
+
+
+pygame.init()
+
+windowSurface = pygame.display.set_mode((500,400),0,32)
+pygame.display.set_caption('Hello World!')
+
+
+Black = (0,0,0)
+White = (255,255,255)
+Red = (255,0,0)
+Green = (0,255,0)
+Blue = (0,0,255)
+
+basicFont = pygame.font.sysFont(None,48)
+
+text = basicFont.render('Hello World!', True, White, Blue)
+textRect = text.get_rect()
+textRect.centerx = windowSurface.get_rect().centerx
+textRect.centery = windowSurface.get_rect().centery
+
