47b6098a3d1f7b142a3c73c31f56fed71f786e5b
BenjaminHightower <behi7195@colorado.edu>
2024-05-04 18:00:55 -0600

    4
    3

--- master_arrow.py
+++ master_arrow.py
@@ -1,9 +1,13 @@
-# space image source: https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fspace%2520background%2F&psig=AOvVaw3J25w2RRPNARpyiLT2cJOu&ust=1713304119399000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPDPh62ZxYUDFQAAAAAdAAAAABAE
+# SOURCES
+# arduino + python help: https://projecthub.arduino.cc/ansh2919/serial-communication-between-python-and-arduino-663756
+# 3d Rocket Model: https://www.myminifactory.com/object/3d-print-gcreate-official-rocket-ship-55463
 # galaxy image source: https://www.peakpx.com/en/hd-wallpaper-desktop-awntk 
 # spaceship image source: https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fillustrations%2Fspaceship&psig=AOvVaw2vckoaUA3y9-mDd2MJoRlV&ust=1713304202156000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOi2tNSZxYUDFQAAAAAdAAAAABAE
 # comet image link: https://www.google.com/url?sa=i&url=https%3A%2F%2Fm.youtube.com%2Fwatch%3Fv%3DmbZfhJeNx7c&psig=AOvVaw2X7D_DW_XsBBgWmRc-EYCy&ust=1713461913505000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCPj_nJflyYUDFQAAAAAdAAAAABAE
 # background music link: https://www.youtube.com/watch?v=fnOv8MvTukQ
 # asteroid image source: https://www.cleanpng.com/png-planet-cartoon-green-moon-466868/ 
+# dog bone image: https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Ftoy-bone-icon-chewing-puppy-treat-symbol_34264712.htm&psig=AOvVaw08b2UQ-5aNgRRAzTd7OiFH&ust=1713572817492000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCND3h7CCzYUDFQAAAAAdAAAAABAE
+# dog head image: https://www.google.com/imgres?imgurl=https%3A%2F%2Fthumbs.dreamstime.com%2Fz%2Fshiba-inu-puppy-face-set-cute-cartoon-icon-logo-happy-dog-tongue-sticking-out-simple-vector-illustration-128772666.jpg&tbnid=dfbviOVtMiBMMM&vet=10CBwQxiAoBGoXChMI8Jj2hbLbhQMVAAAAAB0AAAAAEAc..i&imgrefurl=https%3A%2F%2Fwww.dreamstime.com%2Fshiba-inu-puppy-face-set-cute-cartoon-icon-logo-happy-dog-tongue-sticking-out-simple-vector-illustration-image128772666&docid=a6ygET3GYbyWxM&w=1600&h=1690&itg=1&q=brown%20shiba%20inu%20clip%20art&ved=0CBwQxiAoBGoXChMI8Jj2hbLbhQMVAAAAAB0AAAAAEAc
 # comet hit sound effect: https://www.youtube.com/watch?v=wOh41654QFg
 # asteroid hit sound effect: https://www.youtube.com/watch?v=h3cZFqppGRE
 # bone pickup sound effect: https://www.youtube.com/watch?v=iJe4k2AMOk4
@@ -159,7 +163,7 @@ bones = pygame.sprite.Group()
 asteroids = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
-for i in range(12):
+for i in range(5):
     c = Comet()
     all_sprites.add(c)
     comets.add(c)
@@ -254,7 +258,7 @@ while running:
 
             
         if timeAlive % 500 == 0:
-            for i in range(2):  
+            for i in range(1):  
                 c = Comet()
                 all_sprites.add(c)
                 comets.add(c)
